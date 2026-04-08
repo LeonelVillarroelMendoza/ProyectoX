bc01c0a4458f26e8e844955c7a506e1734637853
LeonelVillarroelMendoza <villarroelmendozaleonel@pedropoveda.edu.bo>
2026-04-08 11:30:31 -0400

    read.php

---
+++ reaad.php
@@ -0,0 +1,20 @@
+<?php
+$direccion="localhost";
+$usuario="root";
+$contraseña="";
+$nombreBase="TaskFlow";
+
+$conexion= new mysqli($direccion,$usuario,$contraseña,$nombreBase);
+if($conexion->error){
+echo "Hubo un error al conectar a la base de datos";
+}
+$sql="SELECT * FROM personas";
+$resultado = $conexion->query($sql);
+if ($resultado->num_rows>0){
+while($fila=$resultado->fetch_assoc()){
+echo $fila['id']."<br>".$fila['nombre']."<br>".$fila['descripcion']."<br>".$fila['estado']."<br>";
+$id=$fila['id'];
+echo "<a href='persona.php?id=$id'><button >Mostrar</button></a><br>";
+}
+}
+?>
